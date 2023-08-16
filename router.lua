fc81a2e30e4c88aca834e61cf2949c2827b4abea
Komeil Majidi <k.majidi@esmcell.com>
2023-08-17 01:34:34 +0330

    example for get #23

---
+++ router.lua
@@ -0,0 +1,23 @@
+package.path = package.path .. ";/www/api/?.lua"
+
+
+local routes = {
+    ["/api/router.lua/user/(%d+)"] = "Controllers.user_controller"
+
+}
+
+
+    local path = lighty.env["request.uri"]
+	print(path)
+    for route, controllerName in pairs(routes) do
+        local params = { string.match(path, route) }
+        if #params > 0 then
+            local controller = require(controllerName)
+            return controller:show(tonumber(params[1]),lighty)
+        end
+    end
+
+    lighty.content = "text/plain"
+    lighty.header["Content-Type"] = "text/plain"
+    lighty.content = "Not Found"
+    return 404
