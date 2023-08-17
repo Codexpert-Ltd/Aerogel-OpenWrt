a4b85a7bc8b533e11da2c6ade586cacf0a4fef84
Komeil Majidi <k.majidi@esmcell.com>
2023-08-18 03:13:11 +0330

    response controller #23

---
+++ Controllers/response_controller.lua
@@ -0,0 +1,14 @@
+local cjson = require("cjson");
+function printResponse(statusMessage, statusCode, message, contentType, description, response)
+	local data = {
+		status = statusMessage,
+		code = statusCode,
+		message = message,
+		data = response
+	};
+	local json_data = cjson.encode(data);
+	print("HTTP/1.1 " .. statusCode .. " " .. description);
+	print("Content-Type: " .. contentType);
+	print();
+	print(json_data);
+end;
