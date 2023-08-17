15210d3c3315b2a4ac599c52e470b78dc60c5ad2
Komeil Majidi <k.majidi@esmcell.com>
2023-08-18 03:14:12 +0330

    test token validate temp #23

--- router.lua
+++ router.lua
@@ -1,19 +1,22 @@
 package.path = package.path .. ";/www/api/?.lua";
 local routes = {
-	["/api/router.lua/user/(%d+)"] = "Controllers.user_controller"
+	["/api/user/authenticate/"] = {
+		"Controllers.token_controller",
+		"authenticate_user"
+	},
+	["/api/user/token/"] = {
+		"Controllers.token_controller",
+		"validate_token"
+	}
 };
-local path = lighty.env["request.uri"];
-print(path);
+local request_json = io.read("*a");
+local path = tostring(os.getenv("REQUEST_URI"));
 for route, controllerName in pairs(routes) do
 	local params = {
 		string.match(path, route)
 	};
 	if #params > 0 then
-		local controller = require(controllerName);
-		return controller:show(tonumber(params[1]), lighty);
+		local controller = require(controllerName[1]);
+		local response = _G[controllerName[2]](request_json);
 	end;
 end;
-lighty.content = "text/plain";
-lighty.header["Content-Type"] = "text/plain";
-lighty.content = "Not Found";
-return 404;
